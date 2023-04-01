b850db92e74d17c99eeac44cbd77fb6d5ee57e4b
Carlos Alves <cp193116@gmail.com>
2023-04-01 14:09:30 -0300

    Create index.js

---
+++ JS_Orientacao_Ojetos/index.js
@@ -0,0 +1,22 @@
+class Cliente {
+    nome;
+    cpf;
+    agencia;
+    saldo;
+}
+
+const cliente1 = new Cliente();
+const cliente2 = new Cliente();
+
+cliente1.nome = "Ricardo";
+cliente1.cpf = 11122244455;
+cliente1.agencia = 1001;
+cliente1.saldo = 0;
+
+cliente2.nome = "alice";
+cliente2.cpf = 1112225254455;
+cliente2.agencia = 1031;
+cliente2.saldo = 0;
+
+
+console.log(cliente2)
